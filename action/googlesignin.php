d4a97766fa0a68c09efd23c69a55f6858887d772
Daniel Kleebinder <daniel.kleebinder@gmx.net>
2018-05-07 16:15:09 +0200

    Added curl https post request to google

--- action/googlesignin.php
+++ action/googlesignin.php
@@ -8,6 +8,17 @@ if (!isset($_POST['idtoken'])) {
     exit();
 }
 $google_idtoken = $_POST['idtoken'];
+$url = 'https://www.googleapis.com/oauth2/v3/tokeninfo';
+
 $_SESSION['idtoken'] = $google_idtoken;
+
+$post = ['id_token' => $google_idtoken];
+
 echo $google_idtoken;
+$ch = curl_init('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='.$google_idtoken);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo 'Response:' . $response;
 ?>
